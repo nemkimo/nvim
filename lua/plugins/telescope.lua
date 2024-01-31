4c3e0dc12b76f9d14d5b74c266d02ef0ec9ee366
Tamas Toth <tamas.toth@htecgroup.com>
2024-01-31 11:24:56 +0100

    Add more shortcuts for nvim/telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,12 +4,23 @@ return {
     branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      local builtin = require('telescope.builtin')
+      require('telescope').setup({
+        defaults = {
+          cache_picker = {
+            num_pickers = 5,
+          },
+        },
+      })
+      local builtin = require('telescope.builtin');
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
       vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Git files' })
       vim.keymap.set('n', '<leader>fc', builtin.commands, { desc = 'Find commands' })
+      vim.keymap.set('n', '<leader>fh', builtin.oldfiles, { desc = 'Find recent files' })
+      vim.keymap.set('n', '<leader>fs', builtin.search_history, { desc = 'Find recent searches' })
+      vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'Find references' })
+      vim.keymap.set('n', '<leader>fp', builtin.pickers, { desc = 'Find pickers' })
     end,
   },
 }
